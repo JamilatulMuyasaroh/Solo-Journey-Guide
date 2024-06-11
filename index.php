e9501465ca01d34a3ab2316f582779fbe59ae7fa
fakhiraarr <fakhiraraisa@student.uns.ac.id>
2024-06-11 20:04:20 +0700

    Update

--- index.php
+++ index.php
@@ -116,7 +116,7 @@ else{
                 <div class="row justify-content-center py-5">
                     <div class="col-lg-10 pt-lg-5 mt-lg-5 text-center">
                         <h1 class="display-3 text-white mb-3 animated slideInDown">Enjoy Your Vacation With Us</h1>
-                        --+<p class=" fs-4 text-white mb-4 animated slideInDown">Nikmati Keindahan Wisata bersama Solo Journey Guide Pengalaman Rekreasi yang Tak Terlupakan-</p>
+                        --+<p class=" fs-4 text-white mb-4 animated slideInDown">Nikmati Keindahan Wisata bersama Solo Journey Guide Pengalaman Rekreasi yang Tak Terlupakan.</p>
                         <div class="position-relative w-75 mx-auto animated slideInDown">
                             <input class="form-control border-0 rounded-pill w-100 py-3 ps-4 pe-5" type="text"
                                 placeholder="Eg: Pasar Gede">
